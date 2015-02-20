314fe977a1f39279a6678d2b6f995dc194a5b848
patorash <chariderpato@gmail.com>
2015-02-21 02:18:30 +0900

    ServletHandlerを使ってServlet on Jettyまでやった。

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,11 +5,20 @@ import org.eclipse.jetty.server.handler.AbstractHandler
 import org.eclipse.jetty.server.Request
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import org.eclipse.jetty.servlet.ServletHandler
+import javax.servlet.http.HttpServlet
 
 fun main(args: Array<String>) {
-    // 単純に全てのアクセスをHelloJettyHandlerに丸投げする
     val server = Server(1234)
-    server.setHandler(HelloJettyHandler())
+
+    // 単純に全てのアクセスをHelloJettyHandlerに丸投げする
+    // server.setHandler(HelloJettyHandler())
+
+    // ServeletHandlerを定義して、URLごとにクラスをマッピングして処理を行う
+    val handler = ServletHandler()
+    handler.addServletWithMapping(javaClass<SampleServlet>(), "/sample")
+
+    server.setHandler(handler)
     server.start()
     server.join()
 }
@@ -22,5 +31,11 @@ class HelloJettyHandler : AbstractHandler() {
         response.setStatus(HttpServletResponse.SC_OK)
         baseRequest!!.setHandled(true)
         response.getWriter().println("<h1>Hello Jetty!!</h1>")
+    }
+}
+
+class SampleServlet : HttpServlet() {
+    override fun doGet(req: HttpServletRequest?, resp: HttpServletResponse?) {
+        resp!!.getWriter().println("Servlet on Jetty.")
     }
 }
